ec3d22ded04031080098b47b1cfde08bfac97005
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-12 10:03:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    let newArr = [arr[0], arr[1]]
+    return newArr
+} 
+
+function  returnLastTwoDrivers(arr) {
+    let newArr = [arr[arr.length - 2], arr[arr.length - 1]]
+    return newArr
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mult) {
+    return function (fare) {
+        return mult*fare
+    }
+}
+
+function fareDoubler(fare) {
+    return fare*2
+}
+
+function fareTripler(fare) {
+    return fare*3
+}
+
+function selectDifferentDrivers(arr, callback) {
+    return callback(arr)
+}
